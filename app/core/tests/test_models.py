52e5347fbfe39e6867f07d7c1e22267e21d89446
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2020-10-23 22:34:55 +0100

    indentation bug fix

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,15 +5,15 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-      """Test creating a new user with an email is successful"""
-      email = 'test@email.com'
-      password = 'test123'
-      user = get_user_model().objects.create_user(
-          email=email,
-          password=password
-      )
-      self.assertEqual(user.email, email)
-      self.assertTrue(user.check_password(password))
+        """Test creating a new user with an email is successful"""
+        email = 'test@email.com'
+        password = 'test123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
         """ Test the email for a new user is normalized"""
